d204b9fcc292ef849ecdfe64b99986b08681b7a6
Squidge <nicole.cooke@eonreality.com>
2018-11-15 20:53:20 +0000

    35, 36 and 37 completed
    35. Sub-Menus With Widget Switchers - Added a widget switcher
    
    36. Solution: Styling Our Sub-Menu - Made a join menu and fixed the menu to look good
    
    37. Navigating Menus In C++ - Join and cancel button are now selectable

--- Source/PuzzlePlatform/MenuSystem/MainMenu.cpp
+++ Source/PuzzlePlatform/MenuSystem/MainMenu.cpp
@@ -5,6 +5,10 @@
 // This gets the button header
 #include "Components/Button.h"
 
+// This gets the WidgetSwitcher header
+#include "Components/WidgetSwitcher.h"
+
+
 
 // This is going to be able to call the host and join buttons
 bool UMainMenu::Initialize()
@@ -15,10 +19,24 @@ bool UMainMenu::Initialize()
 
 	// The engine can return null, so this is put in to return null incase
 	// Returning false so that if the null is false then we fail the Initialize
-	if (!ensure(Host != nullptr)) return false;
+	if (!ensure(HostButton != nullptr)) return false;
 
 	// Host button being clicked event
-	Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+	HostButton->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+
+	// The engine can return null, so this is put in to return null incase
+	// Returning false so that if the null is false then we fail the Initialize
+	if (!ensure(JoinButton != nullptr)) return false;
+
+	// Join button being clicked event
+	JoinButton->OnClicked.AddDynamic(this, &UMainMenu::OpenJoinMenu);
+
+	// The engine can return null, so this is put in to return null incase
+	// Returning false so that if the null is false then we fail the Initialize
+	if (!ensure(CancelJoinMenuButton != nullptr)) return false;
+
+	// CancelJoinMenuButton being clicked event
+	CancelJoinMenuButton->OnClicked.AddDynamic(this, &UMainMenu::OpenMainMenu);
 
 	return true;
 }
@@ -99,6 +117,30 @@ void UMainMenu::HostServer()
 		// Use this and call the host button
 		MenuInterface->Host();
 	}
+}
+
+void UMainMenu::OpenJoinMenu()
+{
+	// The engine can return null, so this is put in to return null incase
+	if (!ensure(WidgetMenuSwitcher != nullptr)) return;
+
+	// The engine can return null, so this is put in to return null incase
+	if (!ensure(JoinMenu != nullptr)) return;
+
+	//WidgetMenuSwitcher switch to another widget
+	WidgetMenuSwitcher->SetActiveWidget(JoinMenu);
+}
+
+void UMainMenu::OpenMainMenu()
+{
+	// The engine can return null, so this is put in to return null incase
+	if (!ensure(WidgetMenuSwitcher != nullptr)) return;
+
+	// The engine can return null, so this is put in to return null incase
+	if (!ensure(JoinMenu != nullptr)) return;
+
+	//WidgetMenuSwitcher switch to another widget
+	WidgetMenuSwitcher->SetActiveWidget(MainMenu);
 }
 
 
--- Source/PuzzlePlatform/MenuSystem/MainMenu.h
+++ Source/PuzzlePlatform/MenuSystem/MainMenu.h
@@ -33,14 +33,39 @@ protected:
 private:
 	// This binds the host button in the WBP_MainMenu
 	UPROPERTY(meta = (BindWidget))
-	class UButton* Host;
+	class UButton* HostButton;
 
 	// This binds the join button in the WBP_MainMenu
 	UPROPERTY(meta = (BindWidget))
-	class UButton* Join;
+	class UButton* JoinButton;
 
+	// This binds the CancelJoinMenu button in the WBP_MainMenu
+	UPROPERTY(meta = (BindWidget))
+	class UButton* CancelJoinMenuButton;
+
+	// This binds the WidgetSwitcher in the WBP_MainMenu
+	UPROPERTY(meta = (BindWidget))
+	class UWidgetSwitcher* WidgetMenuSwitcher;
+
+	// This binds the MainMenu button in the WBP_MainMenu
+	UPROPERTY(meta = (BindWidget))
+	class UWidget* MainMenu;
+
+	// This binds the JoinMenu button in the WBP_MainMenu
+	UPROPERTY(meta = (BindWidget))
+	class UWidget* JoinMenu;
+
+	// This binds the HostButton in the WBP_MainMenu
 	UFUNCTION()
 	void HostServer();
+
+	// This binds the OpenJoinMenu in the WBP_MainMenu
+	UFUNCTION()
+	void OpenJoinMenu();
+
+	// This binds the OpenMainMenu in the WBP_MainMenu
+	UFUNCTION()
+	void OpenMainMenu();
 
 	// This points to our MenuInterface
 	IMenuInterface* MenuInterface;
